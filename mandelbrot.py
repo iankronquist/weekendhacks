c72d4c360e65e4307c6015468ec087820942e73e
Ian <iankronquist@teleport.com>
2013-11-04 15:26:47 -0800

    Improve mandelbrot script. Add generated images.

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,13 +4,15 @@ import Image
 
 def main():
 	cellsize = 10
-	mandelbrot(1000)
+	mandelbrot(1000, 2)
 
-def mandelbrot(precision):
-	increment = 0.001
+def mandelbrot(precision, size):
+	assert size < 5
+	size = 10 ** size
+	increment = 1.0/float(size) /10
 	height = 2
 	width = 3.5
-	img = Image.new( 'RGB', (3500,2000), "black") # create a new black image
+	img = Image.new( 'RGB', (35*size,20*size), "black") # create a new black image
 	pixels = img.load() # create the pixel map
 	img.show()
 	for y0 in drange(-1, 1, increment):
@@ -23,7 +25,7 @@ def mandelbrot(precision):
 				y = 2 * x * y + y0
 				x = xtemp
 				iteration += 1
-				pixels[convertx(x0),converty(y0)] = (iteration,10,100)
+				pixels[convertx(x0, size),converty(y0, size)] = (iteration,10,100)
 		print 'Wrote row: ', y0
         	#if you want to periodically save the picture, uncomment the next line.
 		#img.save('mandelbrot.png')
@@ -36,15 +38,15 @@ def drange(start, stop, step):
 		yield r
 		r += step
 
-def convertx(x):
+def convertx(x, size):
 	#print 'initx', x
-	x = (x + 2.5) * 1000
+	x = (x + 2.5) * size * 10
 	#print 'x', x
 	return x
 
-def converty(y):
+def converty(y, size):
 	#print 'inity', y
-	y = (y + 1) * 1000
+	y = (y + 1) * size * 10
 	#print 'y', y
 	return y
 	
